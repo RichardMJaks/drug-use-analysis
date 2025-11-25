1e9b559e8b3187edfe7cccf634929c6f1fdf932e
Richard Miikael Jaks <richardmiikael.jaks@gmail.com>
2025-11-25 15:30:03 +0200

    feat(main): Added a separate data description function, moved rest of the code into main function

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,74 @@
+import sys
+
 import pandas as pd
 import plotnine as pn
 import sklearn as sk
+import numpy as np
 
+sys.path.insert(1, "scripts/custom_modules") # Required to import scripts from another directory
+import data_analysis
+import data_reading
+
+DRUG_TYPES = [
+    "Alcohol",
+    "Amphet",
+    "Amyl",
+    "Benzos",
+    "Caff",
+    "Cannabis",
+    "Choc",
+    "Coke",
+    "Crack",
+    "Ecstasy",
+    "Heroin",
+    "Ketamine",
+    "Legalh",
+    "LSD",
+    "Meth",
+    "Mushrooms",
+    "Nicotine",
+    "Semer",
+    "VSA",
+]
+
+SEPARATOR_STRING = "\n" + "-" * 20 + "\n"
+
+
+def describe_data(data: pd.DataFrame):
+    print("DATA DESCRIPTION")
+
+    print(SEPARATOR_STRING)
+
+    print("DATA")
+    print(data)
+
+    print(SEPARATOR_STRING)
+
+    print("DRUG CLASSIFIER COUNTS")
+    drug_classifier_counts = data_analysis.get_columns_unique_value_counts(data[DRUG_TYPES])
+    print(drug_classifier_counts)
+
+    print(SEPARATOR_STRING)
+
+    # Count not NaN values in columns
+    print("TOTAL VALUES IN COLUMNS")
+    row_counts = data_analysis.get_columns_value_counts(data)
+    print(row_counts)
+
+    print(SEPARATOR_STRING)
+
+    # Count NaN values in columns
+    print("NaN VALUES IN COLUMNS")
+    nan_counts = data_analysis.get_columns_na_counts(data)
+    print(nan_counts)
+    
+    print(SEPARATOR_STRING)
+
+
+if __name__ == '__main__':
+    data = data_reading.read_data("data/drug_consumption.csv")
+
+    converted_data = data_reading.convert_data(data)
+    converted_data = data_reading.convert_drug_usage_classifiers(data, DRUG_TYPES)
+
+    describe_data(data)
